b9d0e5c08380ea9a1f73184b4b894e12209020c4
Aleksander Jasiński <162800396+alek5nder@users.noreply.github.com>
2024-09-22 17:20:10 +0200

    SimpleCode

---
+++ AreaOfTriangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UczeSB
+{
+    internal class AreOfTriangle
+    {
+
+        public void code()
+        {
+            int width = ReadInt("Enter width: ");
+            int height = ReadInt("Enter height: ");
+
+            Console.WriteLine("The area is: " + width * height * 0.5);
+            Console.ReadLine();
+        }
+
+        public int ReadInt(string message)
+        {
+            Console.WriteLine(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+    }
+}
